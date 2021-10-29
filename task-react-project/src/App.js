1d5a073fc02258911f7d4d991b821728a66f8d44
IT-Preacher <sergiy.fesenko.98@gmail.com>
2021-10-29 17:43:37 +0300

    Update for task hooks

--- task-react-project/src/App.js
+++ task-react-project/src/App.js
@@ -1,50 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Navigation from "./components/navigation";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import BookDetail from "./pages/Books/BookDetail.js";
 
-class App extends Component {
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Switch>
+        <Route 
+          path="/" 
+          component={Home} 
+          exact 
+        />
 
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <Switch>
-          <Route 
-            path="/" 
-            component={Home} 
-            exact 
-          />
-
-          <Route
-            path="/books"
-            component={Books}
-            exact
-          />
-          <Route
-            path="/books/:id"
-            component={BookDetail}
-            exact
-          ></Route>
-        </Switch>
-      </div>
-    );
-  }
+        <Route
+          path="/books"
+          component={Books}
+          exact
+        />
+        <Route
+          path="/books/:id"
+          component={BookDetail}
+          exact
+        ></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
-
-{
-  /* <Route 
-            path="/books/:bookId" 
-            component={() => <BookDetail bookList={this.state.bookItem} />}
-            exact
-          >
-          </Route> */
-}
-{
-  /* <BookDetail bookList={this.state.bookItem} /> */
-}
