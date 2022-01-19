333b40531dfc02fc8131c45a2821e8ec21a16257
IT-Preacher <sergiy.fesenko.98@gmail.com>
2022-01-19 17:35:35 +0200

    Update news card. Added pagination and component for empty news container

---
+++ task-articles/src/pages/Home/components/NewsContainer/components/ArticleConteiner/styled.components.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const ArticleContainer = styled.div`
+  width: 280px;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  margin-bottom: 20px;
+
+//   &:hover {
+//       width: 100%;
+//   }
+
+  header {
+    height: 150px;
+    width: 100%;
+    padding: 15px;
+    width: 100%;
+    background-size: cover;
+    color: #fff;
+
+    h4 {
+      text-transform: uppercase;
+      margin: 0;
+      color: #fff;
+    }
+  }
+
+  .article-body {
+    padding: 15px;
+    background-color: #fff;
+    width: 100%;
+    flex-grow: 1;
+
+    .article-date {
+      font-size: 11px;
+      font-weight: 600;
+      color: grey;
+    }
+
+    p {
+      text-align: initial;
+    }
+
+    .article-title {
+    }
+
+    .article-content {
+      padding-bottom: 40px;
+      font-size: 13px;
+      line-height: 1.8;
+    }
+  }
+
+  footer {
+    height: 30px;
+    margin: 10px;
+
+    a {
+        &:hover {
+            font-size: 20px;
+        }
+    }
+  }
+`;
--- task-articles/src/pages/Home/components/NewsContainer/styled.components.js
+++ task-articles/src/pages/Home/components/NewsContainer/styled.components.js
@@ -1,7 +1,9 @@
-.news-container {
+import styled from "styled-components";
+
+export const StyledNewsConteiner = styled.div`
   .news-articles {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
   }
-}
+`;
