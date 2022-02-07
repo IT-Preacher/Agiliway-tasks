44ff634b00e39fdf4804c60f4abd2267dec6a465
IT-Preacher <sergiy.fesenko.98@gmail.com>
2022-02-07 19:28:37 +0200

    Styled component for Page404

--- task-articles/src/pages/Page404/Page404.js
+++ task-articles/src/pages/Page404/Page404.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Page404Container, BigTextSpan } from "./styled.component";
 
 const Page404 = () => {
-    return <div>Page404</div>
-}
+  return (
+    <Page404Container>
+      <BigTextSpan>Page Not Found 404</BigTextSpan>
+      <p>
+        The page you looking for could not be found. It might have been removed,
+        renamed, or did not exist in the firs place.
+      </p>
+    </Page404Container>
+  );
+};
 
 export default Page404;
