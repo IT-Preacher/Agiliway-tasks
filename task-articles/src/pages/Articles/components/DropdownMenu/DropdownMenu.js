2a4ebf33a9f7601f3602c0a55504644c18e590b7
IT-Preacher <sergiy.fesenko.98@gmail.com>
2022-02-15 16:50:28 +0200

    Dropdown from class to function component

--- task-articles/src/pages/Articles/components/DropdownMenu/DropdownMenu.js
+++ task-articles/src/pages/Articles/components/DropdownMenu/DropdownMenu.js
@@ -1,78 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import propTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Menu, Dropdown } from "antd";
-import propTypes from "prop-types";
 import { EditOutlined, DeleteOutlined, ReadOutlined } from "@ant-design/icons";
 
-class DropdownMenu extends React.Component {
-  state = {
-    visible: false,
-  };
+const DropdownMenu = (props) => {
+  const {
+    uuid,
+    article,
+    handleOpenEditModal,
+    handleOpenDeleteModal,
+    getArticleData,
+  } = props;
+  const [visible, setVisible] = useState(false);
 
-  handleMenuClick = (event) => {
+  const handleMenuClick = (event) => {
     if (event.key === "3") {
       this.setState({ visible: false });
     }
   };
 
-  handleVisibleChange = (flag) => {
-    this.setState({ visible: flag });
+  const handleVisibleChange = (flag) => {
+    setVisible(flag);
   };
 
-  render() {
-    const {
-      uuid,
-      article,
-      handleOpenEditModal,
-      handleOpenDeleteModal,
-      getArticleData,
-    } = this.props;
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key="1" icon={<ReadOutlined />}>
+        <Link to={`/articles/${uuid}`}>Read more</Link>
+      </Menu.Item>
+      <Menu.Item
+        key="2"
+        icon={<EditOutlined />}
+        onClick={() => {
+          handleOpenEditModal(uuid);
+          getArticleData(uuid);
+        }}
+      >
+        <span>Edit</span>
+      </Menu.Item>
+      <Menu.Item
+        key="3"
+        icon={<DeleteOutlined />}
+        onClick={() => {
+          handleOpenDeleteModal(article);
+        }}
+      >
+        <span>Delete</span>
+      </Menu.Item>
+    </Menu>
+  );
 
-    const menu = (
-      <Menu onClick={this.handleMenuClick}>
-        <Menu.Item key="1" icon={<ReadOutlined />}>
-          <Link to={`/articles/${uuid}`}>Read more</Link>
-        </Menu.Item>
-        <Menu.Item
-          key="2"
-          icon={<EditOutlined />}
-          onClick={() => {
-            handleOpenEditModal(uuid);
-            getArticleData(uuid);
-          }}
+  return (
+    <div className="dropdown-menu-article">
+      <Dropdown
+        overlay={menu}
+        onVisibleChange={handleVisibleChange}
+        visible={visible}
+      >
+        <a
+          className="ant-dropdown-link"
+          onClick={(event) => event.preventDefault()}
         >
-          <span>Edit</span>
-        </Menu.Item>
-        <Menu.Item
-          key="3"
-          icon={<DeleteOutlined />}
-          onClick={() => {
-            handleOpenDeleteModal(article);
-          }}
-        >
-          <span>Delete</span>
-        </Menu.Item>
-      </Menu>
-    );
-
-    return (
-      <div className="dropdown-menu-article">
-        <Dropdown
-          overlay={menu}
-          onVisibleChange={this.handleVisibleChange}
-          visible={this.state.visible}
-        >
-          <a
-            className="ant-dropdown-link"
-            onClick={(event) => event.preventDefault()}
-          >
-            <img src="https://img.icons8.com/ios-glyphs/30/000000/more.png" />
-          </a>
-        </Dropdown>
-      </div>
-    );
-  }
-}
+          <img src="https://img.icons8.com/ios-glyphs/30/000000/more.png" />
+        </a>
+      </Dropdown>
+    </div>
+  );
+};
 
 DropdownMenu.propTypes = {
   uuid: propTypes.string,
